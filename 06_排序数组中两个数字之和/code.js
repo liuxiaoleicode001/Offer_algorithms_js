610f40dfbc4826946b3a542641c7f60a797c320b
liuxiaolei001 <7711242+liuxiaolei001@user.noreply.gitee.com>
2023-02-01 10:07:04 +0800

    06 排序数组中两个数字之和

---
+++ 06_排序数组中两个数字之和/code.js
@@ -0,0 +1,9 @@
+var twoSum = function(numbers, target) {
+    let p = 0, q = numbers.length - 1
+    while(p < q) {
+        const addition = numbers[p] + numbers[q]
+        if(addition === target) return [p, q]
+        else if(addition < target) p += 1
+        else q -= 1
+    }
+};
